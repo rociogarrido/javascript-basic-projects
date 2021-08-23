4828d7e8eacfa3e32b30b86135b14a437c4d21d0
Rocío Garrido <rocioganon@gmail.com>
2021-08-23 16:10:14 +0200

    add current date functionality

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -21,3 +21,24 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+// console.log(items);
+
+let futureDate = new Date(2021, 8, 23, 16, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+console.log(weekday);
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
